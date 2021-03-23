418abdf5fa4ec218d0513984039566602cc3b5ec
Atharva <79016996+atharva-librax@users.noreply.github.com>
2021-03-22 23:05:38 -0400

    Create Auth0-Integration-Rule.js

---
+++ Auth0-Integration-Rule.js
@@ -0,0 +1,50 @@
+// Main function to handle the ID Verification
+async function LibraXIdVerification(payload_data, callback){
+
+    // Check if Ocp-Apim-Subscription-Key is present
+    if(!configuration.Subscription_Key){
+        console.log('Missing required Ocp-Apim-Subscription-Key. Skipping.');
+        return callback(null);
+    }
+
+    // Check if First Name or Last name exists
+    if (!payload_data.FirstName ||
+        !payload_data.LastName){
+            console.log('Missing First name or Last name');
+            return callback(null);
+    }
+    
+    // Make Post request to LibraX ID Verification Service 
+    const axios = require('axios');
+    let apiResponse;
+    const baseURL = "https://api.librax.ai";
+    
+    let config = {
+        headers: {
+            'Ocp-Apim-Subscription-Key': configuration.Subscription_Key,
+            'Content-Type': 'application/json'
+        }
+    }
+    apiResponse = await axios.post(baseURL + "/id/verify",{
+        FirstName: payload_data.FirstName,
+        LastName: payload_data.LastName,
+        IDPhoto: payload_data.base64encoding
+    }, config)
+    .catch((apiHttpError)=>{
+        return callback(new Error("Error while calling ID Verification API"));
+    });
+    
+    // Check if response status is 200
+    let responseStatus = typeof apiResponse.status === 'number' ? apiResponse.status : 500;
+    if (responseStatus != 200){
+        return callback(new Error("Some error occured"));
+    }
+    
+    // check if IDStatus is undefined if not set it to appropirate value
+    let verifedStatus = typeof apiResponse.data.result.IDStatus === "boolean" ? apiResponse.data.result.IDStatus : false;
+    if (!verifedStatus){
+        return callback(new Error("Unauthorized User"));
+    }
+
+    return callback(null);
+}
